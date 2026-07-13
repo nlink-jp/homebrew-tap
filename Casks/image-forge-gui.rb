e24e4dfe67fddbf4064a5f05eb3f53850fdfa39a
magifd2 <magi@kiwi.ne.jp>
2026-07-14 03:04:27 +0900

    chore: image-forge-gui 0.9.2 (cask)

--- Casks/image-forge-gui.rb
+++ Casks/image-forge-gui.rb
@@ -1,6 +1,6 @@
 cask "image-forge-gui" do
-  version "0.9.1"
-  sha256 "95ca57b90d798d9178f12f305be629083615fcf286d9ee96b9667bba2a8f0a1b"
+  version "0.9.2"
+  sha256 "d64732cbddc7203eb57f2f4614c115f257079730527f72adad7b93c4bd8dc004"
 
   url "https://github.com/nlink-jp/image-forge-gui/releases/download/v#{version}/image-forge-gui-v#{version}-darwin-arm64.zip"
   name "image-forge-gui"
