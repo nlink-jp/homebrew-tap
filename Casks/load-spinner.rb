057d28b2112989db48c670e5ffb335817f55c29b
magifd2 <magi@kiwi.ne.jp>
2026-07-16 08:57:46 +0900

    chore: load-spinner 0.3.1 (cask)

--- Casks/load-spinner.rb
+++ Casks/load-spinner.rb
@@ -1,6 +1,6 @@
 cask "load-spinner" do
-  version "0.3.0"
-  sha256 "916ff28de412e0cb68b5b84f9608bfd0c3fd221b029db4ab47194f8f96c900a9"
+  version "0.3.1"
+  sha256 "6dbc61274e0a27a3b72a0ce7e983fb8335b36c47050adeeec27e541a09f837ff"
 
   url "https://github.com/nlink-jp/load-spinner/releases/download/v#{version}/load-spinner-v#{version}-darwin-arm64.zip"
   name "load-spinner"
