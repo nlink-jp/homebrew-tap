44f1c2512ce6a98d6bde0c1ff059d7d8849a2b7d
magifd2 <magi@kiwi.ne.jp>
2026-08-01 21:15:24 +0900

    chore: zip-porter 0.4.1 (cask)

--- Casks/zip-porter.rb
+++ Casks/zip-porter.rb
@@ -1,6 +1,6 @@
 cask "zip-porter" do
-  version "0.4.0"
-  sha256 "251538277fdb4ec1d9f73df5d7b52691560fcdd2a1a0920e8b7d0d99558062d3"
+  version "0.4.1"
+  sha256 "d1cbbbf44c7128cd8cf2f3f9b9ce6a21c37b93ac4d93764d71c8911e77d4348c"
 
   url "https://github.com/nlink-jp/zip-porter/releases/download/v#{version}/zip-porter-v#{version}-darwin-arm64.zip"
   name "zip-porter"
