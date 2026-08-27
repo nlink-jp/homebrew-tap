6ea06dcb6e874ed4266c48fa45191d835124c891
magifd2 <magi@kiwi.ne.jp>
2026-08-28 02:39:04 +0900

    chore: mail-analyzer-gui 0.3.0 (cask)

--- Casks/mail-analyzer-gui.rb
+++ Casks/mail-analyzer-gui.rb
@@ -1,6 +1,6 @@
 cask "mail-analyzer-gui" do
-  version "0.2.2"
-  sha256 "01740d67152959679a3918f1f92303a5750b13da287151e619a24e19ec0f231f"
+  version "0.3.0"
+  sha256 "f522126848ac2ccce9eb7710634c281b146ef7e7db7fc2bddeca1ba8460e307f"
 
   url "https://github.com/nlink-jp/mail-analyzer-gui/releases/download/v#{version}/mail-analyzer-gui-v#{version}-darwin-arm64.zip"
   name "mail-analyzer-gui"
@@ -9,9 +9,10 @@ cask "mail-analyzer-gui" do
 
   # Developer ID signed + Apple-notarized + stapled .app (Apple Silicon only).
   depends_on arch: :arm64
-  depends_on macos: :big_sur
+  depends_on macos: :sonoma
 
-  app "mail-analyzer-gui.app"
+  app "MailAnalyzerGUI.app"
+  binary "#{appdir}/MailAnalyzerGUI.app/Contents/MacOS/MailAnalyzerGUI", target: "mail-analyzer-gui"
 
   zap trash: [
     "~/Library/Caches/jp.nlink.mail-analyzer-gui",
