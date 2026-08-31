d58623b34989c02f0a642a555f4a5af594f98bef
magifd2 <magi@kiwi.ne.jp>
2026-08-31 12:32:39 +0900

    chore: task-clock-gui 0.1.0 (cask)

---
+++ Casks/task-clock-gui.rb
@@ -0,0 +1,22 @@
+cask "task-clock-gui" do
+  version "0.1.0"
+  sha256 "4e5c066a791a372dc3ca28205f5626f14d0f23d364f23eb29e394d513ef6c894"
+
+  url "https://github.com/nlink-jp/task-clock-gui/releases/download/v#{version}/task-clock-gui-v#{version}-darwin-arm64.zip"
+  name "task-clock-gui"
+  desc "Menu-bar front end for the task-clock scheduler"
+  homepage "https://github.com/nlink-jp/task-clock-gui"
+
+  # Developer ID signed + Apple-notarized + stapled .app (Apple Silicon only).
+  depends_on arch: :arm64
+  depends_on macos: :big_sur
+
+  app "TaskClock.app"
+
+  zap trash: [
+    "~/Library/Caches/jp.nlink.task-clock-gui",
+    "~/Library/Preferences/jp.nlink.task-clock-gui.plist",
+    "~/Library/Saved Application State/jp.nlink.task-clock-gui.savedState",
+    "~/Library/WebKit/jp.nlink.task-clock-gui",
+  ]
+end
