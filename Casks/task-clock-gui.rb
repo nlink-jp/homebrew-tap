e2e9728d8ce04f6bf9c911f9db624d8676177759
magifd2 <magi@kiwi.ne.jp>
2026-09-02 02:50:05 +0900

    chore: task-clock-gui 0.5.0 (cask)

--- Casks/task-clock-gui.rb
+++ Casks/task-clock-gui.rb
@@ -1,6 +1,6 @@
 cask "task-clock-gui" do
-  version "0.4.1"
-  sha256 "b0fddee7c8673bddd54331bc3fab2e41a25d38580dfb9d7a942a290aa93e2826"
+  version "0.5.0"
+  sha256 "d0daad9ae10e5878fb41416ce1ecdc4f97f217e21d98bf99ee17e206255e6e49"
 
   url "https://github.com/nlink-jp/task-clock-gui/releases/download/v#{version}/task-clock-gui-v#{version}-darwin-arm64.zip"
   name "task-clock-gui"
