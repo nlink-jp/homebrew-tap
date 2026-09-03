9483721ada8890cd09ea545648a6cbb92cab415e
magifd2 <magi@kiwi.ne.jp>
2026-09-03 13:29:32 +0900

    chore: gem-usage-lens-gui 0.1.3 (cask)

--- Casks/gem-usage-lens-gui.rb
+++ Casks/gem-usage-lens-gui.rb
@@ -1,6 +1,6 @@
 cask "gem-usage-lens-gui" do
-  version "0.1.2"
-  sha256 "0bce7bc3ce63e02199103f3645101b3f58bb90e1add602bfe7e3a3ff84833c51"
+  version "0.1.3"
+  sha256 "20aaca64cff2a06f4d505f968f7ad7665ef5206de53e2fe31f54695a32fff656"
 
   url "https://github.com/nlink-jp/gem-usage-lens-gui/releases/download/v#{version}/gem-usage-lens-gui-v#{version}-darwin-arm64.zip"
   name "gem-usage-lens-gui"
