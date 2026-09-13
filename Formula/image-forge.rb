c570241c0537bcee748bb156071b3be870238975
magifd2 <magi@kiwi.ne.jp>
2026-09-13 23:49:05 +0900

    chore: image-forge 0.26.1 (formula)

--- Formula/image-forge.rb
+++ Formula/image-forge.rb
@@ -1,8 +1,8 @@
 class ImageForge < Formula
   desc "Local diffusion image-generation engine and model manager"
   homepage "https://github.com/nlink-jp/image-forge"
-  url "https://github.com/nlink-jp/image-forge/releases/download/v0.26.0/image-forge-v0.26.0-darwin-arm64.zip"
-  sha256 "4c365838cd6ace3f80a1701906ed0d349966863714836d1f786e1c0bfa5e3b6c"
+  url "https://github.com/nlink-jp/image-forge/releases/download/v0.26.1/image-forge-v0.26.1-darwin-arm64.zip"
+  sha256 "bc71a16baaf4cae1d1f884abbdd02a0f4767bf33c99b7cb8d230546349a1afdd"
   license "MIT"
 
   # Prebuilt, Developer ID signed + Apple-notarized Apple Silicon binary.
