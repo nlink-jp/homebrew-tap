e87aa11c06fed02bcbfffbc08b2e793693119c7a
magifd2 <magi@kiwi.ne.jp>
2026-09-21 17:05:49 +0900

    chore: active-lens-gui 0.3.2 (cask)

--- Casks/active-lens-gui.rb
+++ Casks/active-lens-gui.rb
@@ -1,6 +1,6 @@
 cask "active-lens-gui" do
-  version "0.3.1"
-  sha256 "7b6488062b5deec3e11b391dea7b7928173ed3ab5cae26f56e5d3acf44274914"
+  version "0.3.2"
+  sha256 "f24dd951a14c91cf11d0649899f36319f62c8e8831ecbf121edafaffbfed4cfb"
 
   url "https://github.com/nlink-jp/active-lens-gui/releases/download/v#{version}/active-lens-gui-v#{version}-darwin-arm64.zip"
   name "active-lens-gui"
