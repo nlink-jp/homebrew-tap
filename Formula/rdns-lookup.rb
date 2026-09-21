8e8d462c15c4fa2a6127b689cb931c4b61f57ec2
magifd2 <magi@kiwi.ne.jp>
2026-09-21 11:29:58 +0900

    chore: rdns-lookup 0.3.0 (formula)

--- Formula/rdns-lookup.rb
+++ Formula/rdns-lookup.rb
@@ -1,8 +1,8 @@
 class RdnsLookup < Formula
   desc "Look up an IP's domains, a domain's subdomains, and reverse CNAMEs"
   homepage "https://github.com/nlink-jp/rdns-lookup"
-  url "https://github.com/nlink-jp/rdns-lookup/releases/download/v0.2.0/rdns-lookup-v0.2.0-darwin-arm64.zip"
-  sha256 "9d2b92a54f59e0727609ec88b708315b489fc593b117b35bc4c0ffb6ae8e3f75"
+  url "https://github.com/nlink-jp/rdns-lookup/releases/download/v0.3.0/rdns-lookup-v0.3.0-darwin-arm64.zip"
+  sha256 "a9ae980f519ffbea20716abc0800c22fd7472e33973274d70c5e2fa02ccf8b62"
   license "MIT"
 
   # Prebuilt, Developer ID signed + Apple-notarized Apple Silicon binary.
