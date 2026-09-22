8175e916985dfee9faec2a378c4f3f1fb56f101b
magifd2 <magi@kiwi.ne.jp>
2026-09-23 06:50:48 +0900

    chore: scat 2.1.1 (formula)

--- Formula/scat.rb
+++ Formula/scat.rb
@@ -1,8 +1,8 @@
 class Scat < Formula
   desc "Slack CLI for services using bot credentials"
   homepage "https://github.com/nlink-jp/scat"
-  url "https://github.com/nlink-jp/scat/releases/download/v2.0.0/scat-v2.0.0-darwin-arm64.zip"
-  sha256 "6a148da3c3ef8c8f9b5798dd6f7eee29258d9a242a95bc60348c8a4cabbb8cbb"
+  url "https://github.com/nlink-jp/scat/releases/download/v2.1.1/scat-v2.1.1-darwin-arm64.zip"
+  sha256 "8d9953cfad8b6adce9e070679f78be0b437d59d0a9a221f1c57bc75f7228cb60"
   license "MIT"
 
   # Prebuilt, Developer ID signed + Apple-notarized Apple Silicon binary.
